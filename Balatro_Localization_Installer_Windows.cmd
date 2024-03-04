d38d6480a0f626b6d80d859d99b1acd7a1b3470a
Furtys <quentin.furtys@gmail.com>
2024-03-04 21:35:58 +0100

    Modify windows script behaviour to be compatible with Steam not installed in default path.

--- Balatro_Localization_Installer_Windows.cmd
+++ Balatro_Localization_Installer_Windows.cmd
@@ -3,63 +3,101 @@
 
 @echo off
 setlocal enabledelayedexpansion
+:: Permet l'utilisation des caractères accentués.
+chcp 65001>nul
+
+set "colorReset=[0m"
+set "resourcesFolder=Balatro_Localization_Resources"
 
 echo =========================================
-echo ==========     BALATRO MOD     ==========
-echo === Installation du pack de langue FR ===
+echo ==             BALATRO MOD             ==
+echo ==  Installation du pack de langue FR  ==
 echo =========================================
 
-set "color_reset=[0m"
-set "dossier_ressources=Balatro_Localization_Resources"
+:: On vérifie l'existence du fichier libraryfolders.vdf dans le chemin d'installation par défaut Steam.
+set "steamLibraryFile=C:\Program Files (x86)\Steam\steamapps\libraryfolders.vdf"
+
+if not exist "!steamLibraryFile!" (
+    :: S'il n'existe pas, on doit demander à l'utilisateur de sélectionner le fichier Balatro.exe lui même.
+    echo ==========================================
+    echo ==   Steam n'est pas installé dans C:   ==
+    echo ==  Merci de bien vouloir sélectionner  ==
+    echo ==        le fichier Balatro.exe        ==
+    echo ==========================================
+    
+    :: Explorateur de fichier pour que l'utilisateur puisse facilement selectionner le fichier.
+    set "balatroFile="
+    set "dialogTitle=Selectionner balatro.exe"
+    set "fileFilter=Balatro Executable (balatro.exe) | balatro.exe"
+
+    for /f "delims=" %%I in ('powershell -Command "& { Add-Type -AssemblyName System.Windows.Forms; $dlg = New-Object System.Windows.Forms.OpenFileDialog; $dlg.Filter = '!fileFilter!'; $dlg.Title = '!dialogTitle!'; $dlg.ShowHelp = $true; $dlg.ShowDialog() | Out-Null; $dlg.FileName }"') do set "selectedFile=%%I"
+    
+    if defined selectedFile (
+        set "balatroFile=!selectedFile!"
+        echo Balatro.exe selectionne : !balatroFile!
+    ) else (
+        echo == Fichier Balatro.exe non sélectionné  ==
+        echo ==========================================
+        goto :fin
+    )
+)
 
 :: Si le dossier ressources n'existe pas on le cree.
-if not exist "%dossier_ressources%" mkdir "%dossier_ressources%"
+if not exist "%resourcesFolder%" mkdir "%resourcesFolder%"
 
 :: On recupère le dernier tag de la release de Balamod.
 for /f %%a in ('powershell -command "$tag = (Invoke-RestMethod -Uri 'https://api.github.com/repos/UwUDev/balamod/releases/latest').tag_name; $tag"') do set latestTag=%%a
 
-:: Creation des noms et liens des fichiers. Valable uniquement tant que le fichier windows s'appelle bien balamod-v.y.z.windows.exe.
+:: Creation des noms et liens des fichiers. Valable uniquement tant que le fichier windows s'appelle bien balamod-v.y.z-windows.exe.
 set "balamodFile=balamod-%latestTag%-windows.exe"
 set "balamodFileUrl=https://github.com/UwUDev/balamod/releases/download/%latestTag%/%balamodFile%"
 set "FR_translationUrl=https://raw.githubusercontent.com/FrBmt-BIGetNouf/balatro-french-translations/main/localization/fr.lua"
 
-:: Si le fichier balamod n'existe pas on le telecharge.
-if not exist "%dossier_ressources%\%balamodFile%" (
+:: Si le fichier balamod n'existe pas on le télécharge.
+if not exist "%resourcesFolder%\%balamodFile%" (
     echo =========================================
-    echo ======= Telechargement de BALAMOD =======
+    echo ==      Téléchargement de Balamod      ==
     echo =========================================
 
-    curl -L -o "%dossier_ressources%\%balamodFile%" %balamodFileUrl%
+    curl -L -o "%resourcesFolder%\%balamodFile%" %balamodFileUrl%
 
     echo =========================================
-    echo ======== Telechargement termine. ========
+    echo ==       Téléchargement terminé.       ==
     echo =========================================
 ) else (
     echo =========================================
-    echo =======   BALAMOD a jour trouve   =======
+    echo ==        Balamod à jour trouvé        ==
     echo =========================================
 )
 
-:: On telecharge tout le temps la traduction française.
+:: On télécharge tout le temps la traduction française.
 echo =========================================
-echo == Telechargement du fichier de langue ==
+echo == Téléchargement du fichier de langue ==
 echo =========================================
 
-curl -L -o "%dossier_ressources%\fr.lua" %FR_translationUrl%
+curl -L -o "%resourcesFolder%\fr.lua" %FR_translationUrl%
 
 echo =========================================
-echo ======== Telechargement termine. ========
+echo ==       Téléchargement terminé.       ==
 echo =========================================
 
 :: Execution de l'injection du fichier de langue.
 echo =========================================
-echo === Installation du fichier de langue ===
+echo ==  Installation du fichier de langue  ==
 echo =========================================
 
-"./%dossier_ressources%\%balamodFile%" -x -i "%dossier_ressources%\fr.lua" -o localization/fr.lua
+if not defined balatroFile (
+    :: Si Steam installé par défaut, on laisse Balamod chercher le fichier Balatro.
+    "./%resourcesFolder%\%balamodFile%" -x -i .\%resourcesFolder%\fr.lua -o localization/fr.lua
+) else (
+    :: Sinon on lui envoie le dossier du fichier Balatro.exe selectionné précédemment.
+    for %%A in ("!balatroFile!") do set "balatroFolder=%%~dpA"
+    "./%resourcesFolder%\%balamodFile%" -b !balatroFolder! -x -i .\%resourcesFolder%\fr.lua -o localization/fr.lua
+)
 
-echo %color_reset%=========================================
-echo ========= Installation terminee =========
+
+echo %colorReset%=========================================
+echo ==        Installation terminée        ==
 echo =========================================
 
 :: Suppression des fichiers ressources si l'utilisateur veut.
@@ -67,8 +105,11 @@ choice /C ON /M "Voulez-vous supprimer les fichiers telecharges ?"
 if errorlevel 2 goto :fin
 
 :: Si oui, on supprime.
-rd /s /q "%dossier_ressources%"
+rd /s /q "%resourcesFolder%"
 echo Suppression des fichiers terminee
 
 :fin
+echo =========================================
+echo ==   Fin du programme d'installation   ==
+echo =========================================
 pause
--- readme.md
+++ readme.md
@@ -30,12 +30,13 @@ Thanks to :
 1. **Fermez Balatro**
 2. Téléchargez le fichier [Balatro_Localization_Installer_Windows.cmd](https://github.com/Furtys/Balatro_Localization_Installer/blob/main/Balatro_Localization_Installer_Windows.cmd) (`Ctrl`+`Shift`+`S`)
 3. Double cliquer sur le fichier `Balatro_Localization_Installer_Windows.cmd`.
-4. Le script télécharge et installe automatiquement la dernière version de Balamod et le dernier fichier de traduction.
-5. A la fin de l'installation, vous pouvez choisir de supprimer ou non les fichiers téléchargés. (à la prochaine execution, seul le fichier de traduction sera re-téléchargé si Balamod n'a pas reçu de mise à jour et que les fichiers n'ont pas été supprimés).
-6. Vous pouvez maitenant relancer Balatro, sélectionner la langue Française et profiter !
+4. Si Steam n'est pas installé dans `C:\Program Files (x86)\Steam`, le programme vous demande de sélectionner le fichier Balatro.exe.
+5. Le script télécharge et installe automatiquement la dernière version de Balamod et le dernier fichier de traduction.
+6. A la fin de l'installation, vous pouvez choisir de supprimer ou non les fichiers téléchargés. (à la prochaine execution, seul le fichier de traduction sera re-téléchargé si Balamod n'a pas reçu de mise à jour et que les fichiers n'ont pas été supprimés).
+7. Vous pouvez maitenant relancer Balatro, sélectionner la langue Française et profiter !
 
 > **Important :** Ce script doit être executé à chaque nouvelle mise à jour du jeu.
-
+²
 ## (EN) How to use the script ?
 
 ### Linux (SteamDeck)
@@ -53,9 +54,10 @@ Thanks to :
 1. **Close Balatro**
 2. Download the file Balatro_Localization_Installer_Windows.cmd](https://github.com/Furtys/Balatro_Localization_Installer/blob/main/Balatro_Localization_Installer_Windows.cmd) (`Ctrl`+`Shift`+`S`)
 3. Double click on the file `Balatro_Localization_Installer_Windows.cmd`. 
-4. The script automatically downloads and installs the latest version of Balamod and the latest translation file.
-5. At the end of the installation, you can choose whether or not to delete the downloaded files. (At the next run, only the translation file will be re-downloaded if Balamod has not received an update and the files have not been deleted).
-6. You can now launch Balatro, select the French language and enjoy!
+4. If Steam is not installed in `C:\Program Files (x86)\Steam`, programm ask you to select Balatro.exe file's location.
+5. The script automatically downloads and installs the latest version of Balamod and the latest translation file.
+6. At the end of the installation, you can choose whether or not to delete the downloaded files. (At the next run, only the translation file will be re-downloaded if Balamod has not received an update and the files have not been deleted).
+7. You can now launch Balatro, select the French language and enjoy!
 
 > **Important:** This script must be run every time the game is updated.
 
