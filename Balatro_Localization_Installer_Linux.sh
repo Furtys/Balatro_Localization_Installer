1e549df584f636514b2fd6451734926599509c6b
Furtys <quentin.furtys@gmail.com>
2024-03-04 11:19:47 +0100

    Linux script with French Translation

---
+++ Balatro_Localization_Installer_Linux.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+#Script permettant le telechargement automatique de la dernière version de Balamod (https://github.com/UwUDev/balamod) et 
+# du fichier de langue FR créé par la communauté https://github.com/FrBmt-BIGetNouf/balatro-french-translations/.
+
+color_reset=$'\033[0m'
+
+dossier_ressources=$'Balatro_Localization_Resources'
+
+# Téléchargement de balamod depuis le repo Github.
+download_balamod() {
+    echo "========================================="
+    echo "======= Téléchargement de BALAMOD ======="
+    echo "========================================="
+    # URL du GitHub et de l'api.
+    github_url="https://github.com/UwUDev/balamod"
+    github_api_url="https://api.github.com/repos/UwUDev/balamod"
+
+    # On obtient les données de la dernière release via l'API Github.
+    json_latest_release=$(curl -s "${github_api_url}/releases/latest")
+
+    # Recherche du nom de la dernière release.
+    latest_release=$( echo $json_latest_release | grep -oP 'tag/\K[^"]+')
+
+    # Recherche du nom du fichier linux dans la dernière release (valable tant que UwUDev laisse linux dans le nom du fichier).
+    balamod_linux_file=$( echo $json_latest_release | jq -r '.assets[] | select(.name | contains("linux")).name')
+
+    # URL de téléchargement du fichier.
+    linux_file_url="${github_url}/releases/download/${latest_release}/${balamod_linux_file}"
+
+    # Vérifie si le fichier existe déjà dans le dossier courant
+    if [ -e "${dossier_ressources}/${balamod_linux_file}" ]; then
+            echo "========================================="
+            echo "${balamod_linux_file} existe déjà dans le dossier courant."
+    else
+        # Télécharge le fichier Linux
+        curl --create-dirs -o "${dossier_ressources}/${balamod_linux_file}" -LJ "${linux_file_url}"
+        chmod +x "${dossier_ressources}/${balamod_linux_file}"
+        echo "========================================="
+        echo "======== Téléchargement terminé. ========"
+    fi
+    echo "========================================="
+}
+
+# Telechargement de la traduction FR pour Balatro.
+download_fr_lua() {
+    echo "========================================="
+    echo "== Telechargement du fichier de langue =="
+    echo "========================================="
+    # URL du fichier fr.lua
+    URL_fichier="https://raw.githubusercontent.com/FrBmt-BIGetNouf/balatro-french-translations/main/localization/fr.lua"
+
+    # Télécharge le fichier fr.lua
+    echo "Téléchargement du fichier fr.lua"
+    curl --create-dirs -o "${dossier_ressources}/fr.lua" -LJ "${URL_fichier}"
+    echo "========================================="
+    echo "======== Téléchargement terminé. ========"
+    echo "========================================="
+}
+
+# Fonction pour injecter le fichier fr.lua dans Balatro
+inject_file() {
+    # Vérifie si le fichier balamod-linux existe
+    if [ -e "${dossier_ressources}/${balamod_linux_file}" ] && [ -e "${dossier_ressources}/fr.lua" ]; then
+            echo "========================================="
+            echo "=== Installation du fichier de langue ==="
+            echo "========================================="
+        ./$dossier_ressources/$balamod_linux_file -x -i $dossier_ressources/fr.lua -o localization/fr.lua
+    else
+        echo "Le fichier ${balamod_linux_file} n'a pas été trouvé. Une erreur lors du téléchargement a du se produire."
+    fi
+    echo "${color_reset}========================================="    
+    echo "== Voulez-vous supprimer les fichiers  =="
+    echo "==         téléchargés ? [O/N]         =="
+    read -r saisie
+
+    if [[ "$saisie" =~ ^[Oo]$ ]]; then
+        echo "Suppression des fichiers..."
+        rm -R $dossier_ressources
+    elif [[ "$saisie" =~ ^[Nn]$ ]]; then
+        echo "Aucun fichier ne sera supprimé."
+    else
+        echo "Entrée non valide. Aucun fichier n'a été supprimé."
+    fi
+    echo "========================================="
+    echo "========= Installation terminée ========="
+    echo "========================================="
+}
+
+echo "========================================="
+echo "==========     BALATRO MOD     =========="
+echo "=== Installation du pack de langue FR ==="
+echo "========================================="
+
+download_balamod
+download_fr_lua
+inject_file
---
+++ readme.md
@@ -0,0 +1,48 @@
+
+[![Balatro](https://www.playbalatro.com/assets/logo2-C9SU2BrI.png)](https://www.playbalatro.com/)
+
+# Balatro Translations Auto Install (Work in progress)
+
+A simple script to auto install localization of Balatro (only French for the moment).
+
+If you'd like to see your translation files added to the tool, feel free to send me the link to these files and I'll upgrade the script to add a language choice.
+
+More informations about Balatro on [https://www.playbalatro.com/](https://www.playbalatro.com/), buy it!
+
+Thanks to :
+- UwUDev for his insane tool Balamod : [https://github.com/UwUDev/balamod](https://github.com/UwUDev/balamod)
+- French Community for their incredible work on this new French translation : [https://github.com/FrBmt-BIGetNouf/balatro-french-translations/](https://github.com/FrBmt-BIGetNouf/balatro-french-translations/) 🇨🇵
+
+## (FR) Comment utiliser le script ?
+
+### Linux (SteamDeck)
+1. **Fermez Balatro**
+2. Passez en mode bureau.
+3. Téléchargez le script [Balatro_Localization_Installer_Linux.sh](https://github.com/Furtys/Balatro_Localization_Installer/blob/main/Balatro_Localization_Installer_Linux.sh) (`Ctrl`+`Shift`+`S`)
+4. Faire clic-droit sur le script `Balatro_Localization_Installer_Linux.sh` et choisir `Run In Konsole`
+5. Le script télécharge et installe automatiquement la dernière version de Balamod et le dernier fichier de traduction.
+6. A la fin de l'installation, vous pouvez choisir de supprimer ou non les fichiers téléchargés. (à la prochaine execution, seul le fichier de traduction sera re-téléchargé si Balamod n'a pas reçu de mise à jour et que les fichiers n'ont pas été supprimés).
+7. Vous pouvez maitenant relancer Balatro, sélectionner la langue Française et profiter !
+
+> **Important :** Ce script doit être executé à chaque nouvelle mise à jour du jeu.
+
+### Windows
+[WIP]
+
+## (EN) How to use the script ?
+
+### Linux (SteamDeck)
+1. **Close Balatro**
+2. Switch to desktop mode.
+3. Download the script [Balatro_Localization_Installer_Linux.sh](https://github.com/Furtys/Balatro_Localization_Installer/blob/main/Balatro_Localization_Installer_Linux.sh) (`Ctrl`+`Shift`+`S`)
+4. Right click on the script `Balatro_Localization_Installer_Linux.sh` and choose `Run In Konsole` 
+5. The script automatically downloads and installs the latest version of Balamod and the latest translation file.
+6. At the end of the installation, you can choose whether or not to delete the downloaded files. (At the next run, only the translation file will be re-downloaded if Balamod has not received an update and the files have not been deleted).
+9. You can now launch Balatro, select the French language and enjoy!
+
+> **Important:** This script must be run every time the game is updated.
+
+### Windows
+[WIP]
+
+By Furtys
